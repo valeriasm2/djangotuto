56ff6f7be6e2481e8a48ba561239110c3bb54b2a
valeria <vsantanamegia01.cf@iesesteveterradas.cat>
2026-02-20 16:54:34 +0100

    clase vier20

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+# views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Question, Choice
 
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]  # las 5 preguntas más recientes
-    context = {"latest_question_list": latest_question_list}
-    return render(request, "polls/index.html", context)
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_questions': latest_questions})
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "No seleccionaste nada."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return redirect('polls:results', question_id=question.id)
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
